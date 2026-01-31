a14f4175a679ed3b0e6fe00f2e561c5ba5095365
Karthik G. Appiah <karthikgappiah@gmail.com>
2026-01-30 19:52:19 -0800

    chore: designed sign in page (see #9)

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -5,12 +5,11 @@ export default function SignInPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        {/** biome-ignore lint/a11y/useValidAnchor: <explanation> */}
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
+        <a href="/" className="flex items-center gap-2 self-center font-medium">
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
             <LinktreeLogoIcon className="size-4" />
           </div>
-          Acme Inc.
+          AppiahCo
         </a>
         <LoginForm />
       </div>
