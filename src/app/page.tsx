7e71390a35cb6e41b64332d6e3a65f3384bfd618
Karthik G. Appiah <karthikgappiah@gmail.com>
2026-01-30 17:16:14 -0800

    feat: set up shadcn ui (closes #5)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/shadcn/button";
+
 export default function Page() {
   return (
-    <h1 className="h-screen flex items-center justify-center">
-      {"Sorry, this website is under construction!"}
-    </h1>
+    <main className="h-screen flex items-center justify-center">
+      <Button>
+        <p>{"Sorry, this website is under construction!"}</p>
+      </Button>
+    </main>
   );
 }
